f68ff1703c3e48a167876d38947a1d14d98eb66b
Komolafe Elisha <34249609+eak0mo@users.noreply.github.com>
2026-04-24 14:46:40 +0200

    return working oscbf folder

---
+++ oscbf/examples/test.py
@@ -0,0 +1,10 @@
+import sys
+# import os
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+# sys.path.insert(1,os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append("././")
+
+from package.pack import test
+
+
+test()
